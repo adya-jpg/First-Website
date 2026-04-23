002e493125cf3914af369ec8fc707090e2ed0047
adya verma <adya.v3rma@gmail.com>
2026-04-23 11:41:04 -0400

    binary calc edits

--- binarycalc.java
+++ binarycalc.java
@@ -1,14 +1,19 @@
 //this is a project that takes user input as binary and then converts it to decimal
+//it uses dialog boxes to get the input and display the output
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class binarycalc {
+
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter a binary number: ");
-            String binaryString = scanner.nextLine();
-            int decimalValue = Integer.parseInt(binaryString, 2);
-            System.out.println("The decimal value is: " + decimalValue);
-        }
+
+        //get user input
+        String binary = JOptionPane.showInputDialog("Enter a binary number: ");
+
+        //convert binary to decimal
+        int decimal = Integer.parseInt(binary, 2);
+        
+        //display the result
+        JOptionPane.showMessageDialog(null, "The decimal value of " + binary + " is: " + decimal);
     }
 }
